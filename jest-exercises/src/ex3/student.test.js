2940603ea5d0ea5157dd05c5a62de7465d462170
Kamil Richert <krichert@atlassian.com>
2024-04-04 11:40:39 +0200

    Jest exercises: ex3 done

--- jest-exercises/src/ex3/student.test.js
+++ jest-exercises/src/ex3/student.test.js
@@ -2,28 +2,32 @@
 
 import { validateStudent } from './student';
 
+const createStudent = (name) => ({
+    name
+})
+
 describe('validateStudent', () => {
     beforeEach(() => {
         expect.hasAssertions();
     });
 
     test('should be valid, when name contains only letters', () => {
-
+        expect(validateStudent(createStudent('Onlyletters'))).toBeTruthy();
     });
 
     test('should be valid, when name is Marcin', () => {
-
+        expect(validateStudent(createStudent('Marcin'))).toBeTruthy();
     });
 
     test('should be invalid, when name contains spaces', () => {
-
+        expect(validateStudent(createStudent('M a r c i n'))).toBeFalsy();
     });
 
     test('should be invalid, when name contains numbers', () => {
-
+        expect(validateStudent(createStudent('Marcin96'))).toBeFalsy();
     });
 
     test('should invalid, when name is written with big letters', () => {
-
+        expect(validateStudent(createStudent('MARCIN'))).toBeFalsy();
     });
 });
