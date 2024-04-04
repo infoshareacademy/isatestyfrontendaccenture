4f16dda40bc2cc823c002e54e66f58c9f6c1885e
Kamil Richert <krichert@atlassian.com>
2024-04-04 09:20:23 +0200

    Exercises: ex1 done

--- exercises/ex1/math.test.js
+++ exercises/ex1/math.test.js
@@ -13,31 +13,70 @@ describe('add', () => {
     });
 
     test('should return negative value if both numbers are negative', () => {
-        throw ('No test')
+        const actual = add(-1, -3);
+
+        if (actual === -4) {
+            return;
+        } else {
+            throw new Error('It should be -4')
+        }
     });
 });
 
 
 describe('factorial', () => {
     test('should return 6 when arguemnt is 3', () => {
-        throw ('No test')
+        const actual = factorial(3);
+
+        if (actual === 6) {
+            return;
+        } else {
+            throw new Error('It should be 6')
+        }
     });
 
     test('should return 1 when arguemnt is 1', () => {
-        throw ('No test')
+        const actual = factorial(1);
+
+        if (actual === 1) {
+            return;
+        } else {
+            throw new Error('It should be 6')
+        }
     });
 
     test('should return 1 when arguemnt is 0', () => {
-        throw ('No test')
+        const actual = factorial(0);
+
+        if (actual === 1) {
+            return;
+        } else {
+            throw new Error('It should be 6')
+        }
     });
 });
 
 describe('divide', () => {
     test('should return error when second value is 0', () => {
-        throw ('No test')
+        try {
+            divide(2, 0);
+            throw new Error('It should return error')
+        } catch (err) {
+            if (err.message === 'Second numbers have to be positive.') {
+                return;
+            } else {
+                throw new Error('It should return error')
+            }
+        }
     });
 
     test('should return correct value when both numbers are positive', () => {
-        throw ('No test')
+        const actual = divide(6, 3);
+
+        if (actual === 2) {
+            return;
+        } else {
+            throw new Error('It should be 2')
+        }
     });
 });
