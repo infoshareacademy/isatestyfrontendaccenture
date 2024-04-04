08d7c4edd6ec49c005b660f48e403466f39deadf
Kamil Richert <krichert@atlassian.com>
2024-04-04 13:47:45 +0200

    Jest mock exercises: ex1 done option 2

--- jest-mock-exercises/src/ex1/generator.test.js
+++ jest-mock-exercises/src/ex1/generator.test.js
@@ -1,18 +1,23 @@
 // Zrób mock funckji getRandomNumber by zawsze zwracał taka wartość jaką potrzebuijesz i napisz testy.
 import { generateId } from './generator';
+import { getRandomNumber } from './math';
 
 jest.mock('./math', () => ({
-    getRandomNumber: () => 50
+    getRandomNumber: jest.fn()
 }))
 
 describe('generateId', () => {
-    beforeEach(() => expect.hasAssertions())
+    beforeEach(() => {
+        expect.hasAssertions();
+        getRandomNumber.mockReturnValue(50);
+    })
 
     test('should generate id with given seed', () => {
         expect(generateId('accenture')).toBe('id_accenture-50')
     });
 
     test('should generate id with given empty seed', () => {
-        expect(generateId('')).toBe('id_-50');
+        getRandomNumber.mockReturnValue(100);
+        expect(generateId('')).toBe('id_-100');
     });
 });
