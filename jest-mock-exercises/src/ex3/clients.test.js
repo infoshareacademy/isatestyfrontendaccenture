c1e28ad63882dd6a5bff425895d2e54af46ed74d
Kamil Richert <krichert@atlassian.com>
2024-04-04 14:33:05 +0200

    Jest mock exercises: ex3 done

--- jest-mock-exercises/src/ex3/clients.test.js
+++ jest-mock-exercises/src/ex3/clients.test.js
@@ -12,26 +12,64 @@ const mockClientsResponse = [
     { id: 5, name: 'Janina', type: 'INTERNAL', registered: new Date(2019, 11, 3) },
 ];
 
+jest.mock('./clients-data.js', () => ({
+    getClients: jest.fn()
+}));
+
+jest.mock('./logger', () => ({
+    logger: {
+        error: jest.fn()
+    }
+}))
+
 describe('filterClients', () => {
     beforeEach(() => expect.hasAssertions())
+    afterEach(() => {
+        jest.resetAllMocks()
+    })
 
     test('should filter only INTERNAL clients for INTERNAL type', async () => {
+        getClients.mockReturnValue(Promise.resolve(mockClientsResponse));
 
+        const result = await filterClients({ type: 'INTERNAL' });
+
+        expect(result).toEqual([
+            { id: 1, name: 'Jan', type: 'INTERNAL', registered: new Date(2019, 4, 12) },
+            { id: 2, name: 'Maria', type: 'INTERNAL', registered: new Date(2018, 3, 11) },
+            { id: 5, name: 'Janina', type: 'INTERNAL', registered: new Date(2019, 11, 3) },
+        ])
     });
 
     test('should return empty clients when empty list fetched', async () => {
+        getClients.mockReturnValue(Promise.resolve([]));
 
+        const result = await filterClients({ type: 'INTERNAL' });
+
+        expect(result).toEqual([])
     });
 
     test('should return empty list when fetching clients failed', async () => {
+        getClients.mockReturnValue(Promise.reject(new Error('błąd')));
 
+        const result = await filterClients({ type: 'INTERNAL' });
+
+        expect(result).toEqual([])
     });
 
     test('should log error when fetching clients failed', async () => {
+        const error = new Error('błąd');
+        getClients.mockReturnValue(Promise.reject(error));
 
+        const result = await filterClients({ type: 'INTERNAL' });
+
+        expect(logger.error).toHaveBeenCalledWith('fetching clients failed!', error)
     });
 
     test('should not log error when fetching clients successful', async () => {
+        getClients.mockReturnValue(Promise.resolve(mockClientsResponse));
 
+        const result = await filterClients({ type: 'INTERNAL' });
+
+        expect(logger.error).not.toHaveBeenCalled();
     });
 });
