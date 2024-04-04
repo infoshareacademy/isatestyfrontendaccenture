a81a9d4d4679c15d7430fbde54f78d39222a6125
Kamil Richert <krichert@atlassian.com>
2024-04-04 11:56:19 +0200

    Jest exercises: ex4 done

--- jest-exercises/src/ex4/clients.test.js
+++ jest-exercises/src/ex4/clients.test.js
@@ -9,15 +9,21 @@ describe('filterClients', () => {
     });
 
     test('should filter INTERNAL clients', () => {
+        const actual = filterClients(clients, { type: 'INTERNAL' });
 
+        expect(actual).toHaveLength(3);
     });
 
     test('should filter EXTERNAL clients', () => {
+        const actual = filterClients(clients, { type: 'EXTERNAL' });
 
+        expect(actual).toHaveLength(2);
     });
 
     test('should not filter when filter type is unknown', () => {
+        const actual = filterClients(clients, { type: 'UNKNOWN' });
 
+        expect(actual).toHaveLength(5);
     });
 });
 
@@ -27,10 +33,17 @@ describe('getLatestRegisteredClient', () => {
     });
 
     test('should return client with latest registered date', () => {
+        const actual = getLatestRegisteredClient(clients);
 
+        expect(actual.name).toBe('Andrzej')
     });
 
     test('should return first client when there are two clients with the same date', () => {
+        const actual = getLatestRegisteredClient([
+            { id: 6, name: 'Kunegunda', type: 'INTERNAL', registered: new Date(2020, 10, 9) },
+            ...clients
+        ]);
 
+        expect(actual.name).toBe('Kunegunda')
     });
 });
