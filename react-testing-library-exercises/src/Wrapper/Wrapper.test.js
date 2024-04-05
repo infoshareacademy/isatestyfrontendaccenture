f89dcb1b2392cc2583f05df7cfd89d29a97299db
Kamil Richert <krichert@atlassian.com>
2024-04-05 09:47:59 +0200

    React-testing-library Wrapper ex done

--- react-testing-library-exercises/src/Wrapper/Wrapper.test.js
+++ react-testing-library-exercises/src/Wrapper/Wrapper.test.js
@@ -1,14 +1,25 @@
 // Napisz podane testy!
+import { render, screen } from '@testing-library/react';
 import { Wrapper } from './Wrapper';
+
 
 describe('Wrapper', () => {
     beforeEach(() => expect.hasAssertions());
 
     test('should create div with correct styles', () => {
+        const { container } = render(<Wrapper />);
 
+        expect(container.firstChild).toMatchSnapshot();
     });
 
     test('should show passed children', () => {
+        const Child = () => <div>Child</div>;
+        render(
+            <Wrapper>
+                <Child />
+            </Wrapper>
+        );
 
+        expect(screen.getByText('Child')).toBeInTheDocument();
     });
 });
