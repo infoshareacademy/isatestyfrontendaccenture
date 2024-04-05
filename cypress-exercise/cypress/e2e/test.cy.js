c67cfac83381a77c70bc08c6e763bb73b8ce40bb
Kamil Richert <krichert@atlassian.com>
2024-04-05 15:46:50 +0200

    Cypress page object model

--- cypress-exercise/cypress/e2e/test.cy.js
+++ cypress-exercise/cypress/e2e/test.cy.js
@@ -19,16 +19,38 @@ describe.skip('My First Test', () => {
 
 // Postaw aplikacje w trybie deweloperskim i napisz testy.
 
-describe('Classic todo e2e tests', () => {
-    beforeEach(() => {
+const pageTodoList = {
+    prepareTest: () => {
         cy.visit('http://localhost:3000/');
         cy.get('form input').as('input')
+    },
+    addTodoTask: (taskName) => {
+        cy.get('@input').type(`${taskName}{enter}`);
+    },
+    getRemoveButtonByTaskName: (taskName) => {
+        return cy.get('ul li').contains(taskName).parent().find('button')
+    },
+    getCheckboxByTaskName: (taskName) => {
+        return cy.contains(`${taskName}`).parent().find('input[type=checkbox]')
+    },
+    clickShowDone: () => {
+        cy.get('button').contains('Show done').click();
+    },
+    clickHideDone: () => {
+        cy.get('button').contains('Hide done').click();
+    }
+}
+
+describe('Classic todo e2e tests', () => {
+    beforeEach(() => {
+        pageTodoList.prepareTest();
     })
 
     it('should add new todo item', function () {
-        cy.get('@input').type('new task{enter}');
+        const newTask = 'new task';
+        pageTodoList.addTodoTask(newTask);
 
-        cy.contains('new task').should('exist');
+        cy.contains(newTask).should('exist');
     });
 
     it('if there is no todo item then it should show nothing to do', () => {
@@ -36,78 +58,83 @@ describe('Classic todo e2e tests', () => {
     });
 
     it('should remove todo item', function () {
-        cy.get('@input').type('new task{enter}');
+        const newTask = 'new task';
+        pageTodoList.addTodoTask(newTask);
 
         // OPCJA 1
         // cy.contains('🗑').click();
 
         // OPCJA 2
-        cy.get('ul li').first().find('button').click();
+        // cy.get('ul li').first().find('button').click();
 
         // OPCJA 3
         // cy.get('ul li:first-child button').click();
+
+        // OPCJA 4
+        pageTodoList.getRemoveButtonByTaskName(newTask).click();
 
         cy.contains('new task').should('not.exist');
     });
 
     it('should open done task panel when click show done', () => {
-        cy.get('button').contains('Show done').click();
+        pageTodoList.clickShowDone();
 
         cy.get('h2').contains('Done').should('exist');
     });
 
     it('should hide done task panel when click hide done', () => {
-        cy.get('button').contains('Show done').click();
+        pageTodoList.clickShowDone();
 
         cy.get('h2').contains('Done').should('exist');
 
-        cy.get('button').contains('Hide done').click();
+        pageTodoList.clickHideDone();
 
         cy.contains('Done').should('not.exist');
     });
 
     it('should mark todo item as done', () => {
-        cy.get('@input').type('new task{enter}');
+        const newTask = 'new task';
+        pageTodoList.addTodoTask(newTask);
 
-        cy.contains('new task').should('exist');
+        cy.contains(newTask).should('exist');
 
-        cy.contains('new task').parent().find('input[type=checkbox]').click();
+        pageTodoList.getCheckboxByTaskName(newTask).click();
 
-        cy.contains('new task').should('not.exist');
+        cy.contains(newTask).should('not.exist');
 
-        cy.get('button').contains('Show done').click();
+        pageTodoList.clickShowDone()
 
-        cy.contains('new task').should('exist');
-        cy.contains('new task').parent().find('input[type=checkbox]').should('be.checked')
+        cy.contains(newTask).should('exist');
+        pageTodoList.getCheckboxByTaskName(newTask).should('be.checked')
     });
 
     it('should unmark todo item as done', () => {
-        cy.get('@input').type('new task{enter}');
+        const newTask = 'new task';
+        pageTodoList.addTodoTask(newTask);
 
-        cy.contains('new task').should('exist');
-        cy.contains('new task').parent().find('input[type=checkbox]').as('checkbox');
+        cy.contains(newTask).should('exist');
 
-        cy.get('@checkbox').click();
+        pageTodoList.getCheckboxByTaskName(newTask).click();
 
         cy.contains('new task').should('not.exist');
 
-        cy.get('button').contains('Show done').click();
+        pageTodoList.clickShowDone();
 
         cy.contains('new task').should('exist');
 
 
-        cy.get('@checkbox').should('be.checked');
-        cy.get('@checkbox').click();
-
-        cy.get('@checkbox').should('be.not.checked');
+        pageTodoList.getCheckboxByTaskName(newTask).should('be.checked').click();
+        pageTodoList.getCheckboxByTaskName(newTask).should('be.not.checked');
     });
 
     it('should remove todo item from done panel', () => {
-        cy.get('@input').type('new task{enter}');
-        cy.contains('new task').parent().find('input[type=checkbox]').click();
-        cy.get('button').contains('Show done').click();
+        const newTask = 'new task';
+        pageTodoList.addTodoTask(newTask);
 
-        cy.contains('new task').should('exist').parent().find('button').click();
+        pageTodoList.getCheckboxByTaskName(newTask).click();
+        pageTodoList.clickShowDone();
+
+        pageTodoList.getRemoveButtonByTaskName(newTask).click();
 
         cy.contains('new task').should('not.exist')
     });
